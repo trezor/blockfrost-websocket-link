c0061dfc245f8c167ede48e5161f5ba58812503d
Marek Polak <marek.polak@satoshilabs.com>
2026-04-14 13:16:46 +0200

    perf: lighter healthcheck

--- src/utils/health.ts
+++ src/utils/health.ts
@@ -1,12 +1,6 @@
-import { ALL_SEED } from '../scripts/performance/constants.js';
 import { WebsocketClient } from './websocket-client.js';
 
 export const healthCheck = async (url: string) => {
-  // Public key of the account that will be used to perform health check
-  // Account is already used on every network (mainnet, preprod, preview)
-  // One of the account testnet address: addr_test1qrx7xj9qf0j8cqty3eyntpentpgkuhzt9n7g06fa7l29sucle6svh9nacvm632nmcy6fnw9sq85tqkvhagfrhkj9tf6sa3ccwq
-  const ACCOUNT_PUB_KEY =
-    '140791584001446365f169c82241c7c214475000180dab39fa0588fc9c3d6d807f9f812d49816844b52e319857aa75961724ad1a146701679d02d7168622233d';
   const c = new WebsocketClient(url, false);
 
   await c.waitForConnection();
@@ -14,45 +8,32 @@ export const healthCheck = async (url: string) => {
   const serverInfo = await c.sendAndWait('GET_SERVER_INFO');
 
   if (serverInfo.name !== 'Cardano') {
-    console.info('[HealthCheck] Received GET_SERVER_INFO', serverInfo);
     c.close();
+    console.info('[HealthCheck] Received GET_SERVER_INFO', serverInfo);
     throw new Error('[HealthCheck] HealthCheck failed: GET_SERVER_INFO');
   }
 
-  const xpub = ALL_SEED[1];
-
-  const basicAccountInfo = await c.sendAndWait('GET_ACCOUNT_INFO', {
-    descriptor: xpub,
-    details: 'basic',
-    pageSize: 25,
+  const blockInfo = await c.sendAndWait('GET_BLOCK', {
+    hashOrNumber: 12_606_136,
   });
 
-  if (basicAccountInfo.descriptor !== ACCOUNT_PUB_KEY || basicAccountInfo.empty !== false) {
+  if (blockInfo.hash !== 'a3a107cac976e01ba40e15ac3c2db3f421373d63587105166ba01a0ec1538018') {
     c.close();
-    console.info('[HealthCheck] Received GET_ACCOUNT_INFO (basic)', basicAccountInfo);
-    throw new Error('[HealthCheck] HealthCheck failed: GET_ACCOUNT_INFO (basic)');
+    console.info('[HealthCheck] Received GET_BLOCK', blockInfo);
+    throw new Error('[HealthCheck] HealthCheck failed: GET_BLOCK');
   }
 
-  const txsAccountInfo = await c.sendAndWait('GET_ACCOUNT_INFO', {
-    descriptor: xpub,
-    details: 'txs',
-    pageSize: 25,
+  const txInfo = await c.sendAndWait('GET_TRANSACTION', {
+    txId: '8278d71d95762c97278793f407700f5c7f19fad5218273410e5fb883661a2055',
   });
 
-  if (txsAccountInfo.descriptor !== ACCOUNT_PUB_KEY || txsAccountInfo.empty !== false) {
-    console.info('[HealthCheck] Received GET_ACCOUNT_INFO (txs)', txsAccountInfo);
+  if (
+    txInfo.block !== 'a3a107cac976e01ba40e15ac3c2db3f421373d63587105166ba01a0ec1538018' ||
+    txInfo.fees !== '165748'
+  ) {
     c.close();
-    throw new Error('[HealthCheck] HealthCheck failed: GET_ACCOUNT_INFO (txs)');
-  }
-
-  const accountUtxo = await c.sendAndWait('GET_ACCOUNT_UTXO', {
-    descriptor: xpub,
-  });
-
-  if (!Array.isArray(accountUtxo)) {
-    console.info('[HealthCheck] Received GET_ACCOUNT_UTXO', accountUtxo);
-    c.close();
-    throw new Error('[HealthCheck] HealthCheck failed: GET_ACCOUNT_UTXO');
+    console.info('[HealthCheck] Received GET_TRANSACTION', txInfo);
+    throw new Error('[HealthCheck] HealthCheck failed: GET_TRANSACTION');
   }
 
   c.close();
