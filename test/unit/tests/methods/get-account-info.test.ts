36673090482ea5c4985b3c3d29863ffbd3477fa2
Marek Polak <marek.polak@satoshilabs.com>
2026-04-15 17:11:03 +0200

    test: fix get-account-info tests

--- test/unit/tests/methods/get-account-info.test.ts
+++ test/unit/tests/methods/get-account-info.test.ts
@@ -108,7 +108,6 @@ describe('getAccountInfo', () => {
     //     fs.appendFileSync(path.join(__dirname, `./__mocks__/getAccountInfo.nock.ts`), content),
     // });
 
-    loadAssets();
     loadRecord();
     const result = await getAccountInfo(
       '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
@@ -135,7 +134,6 @@ describe('getAccountInfo', () => {
     //     ),
     // });
 
-    loadAssets();
     loadRecordWithCbor();
     const result = await getAccountInfo(
       '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
